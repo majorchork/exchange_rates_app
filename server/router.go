b4de89730ff268c8afde67df584657ba2f7965f9
majorchork <majorchork@gmail.com>
2022-06-30 13:47:23 +0100

    update

--- server/router.go
+++ server/router.go
@@ -5,10 +5,11 @@ import (
 )
 
 func SetupRouter(h *handler) error {
+	//router := http.NewServeMux()
 
-	http.HandleFunc("/rates/getlatest", h.GetLatestExchangeHandler())
+	http.HandleFunc("/rates/latest", h.GetLatestExchangeHandler())
 	http.HandleFunc("/rates/analyze", h.GetAnalyzedRatesHandler())
-	http.HandleFunc("/rates", h.GetExchangeByDateHandler())
+	http.HandleFunc("/rates/", h.GetExchangeByDateHandler())
 	if err := http.ListenAndServe(":8085", nil); err != nil {
 		return err
 	}
